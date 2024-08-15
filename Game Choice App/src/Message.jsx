eeec1f7ad1796d28b51c0365c1d4607546edf845
Julius Peter Ntale <juliusntale30@gmail.com>
2024-08-15 09:35:54 +0300

    Initial commit

---
+++ Game Choice App/src/Message.jsx
@@ -0,0 +1,14 @@
+
+//PascalCasing
+function Message(){
+  
+    //JXC: Javascript XML
+    const name = 'Julius';
+    
+    if (name)
+    return <h1>Hellow {name}</h1>;
+    return <h1>Hellow World</h1>;
+
+}
+
+export default Message;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Game-Choice-Application
+ A Web application where user can chose the game he or she want to buy and be deliver it to him or her 
